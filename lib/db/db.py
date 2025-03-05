a3a10b8cac6ccc1b0aa86c61c6de4d9f97a5514a
OdetoDecode <satapathy.jatin0812+github@gmail.com>
2025-03-05 20:12:02 +0530

    Update db.py

--- lib/db/db.py
+++ lib/db/db.py
@@ -1,5 +1,6 @@
 from os.path import isfile
 from sqlite3 import connect
+from apscheduler.triggers.cron import CronTrigger
 
 DB_PATH = "./data/db/database.db"
 BUILD_PATH = "./data/db/build.sql"
@@ -20,6 +21,9 @@ def build():
 def commit():
     cxn.commit()
 
+def autosave(sched):
+	sched.add_job(commit, CronTrigger(second=0))
+
 def close():
     cxn.close()
     
@@ -36,7 +40,7 @@ def records(command, *values):
     cur.execute(command,tuple(values))
     return cur.fetchall()
 
-def field(command, *values):
+def column(command, *values):
     cur.execute(command,tuple(values))
     return[item[0] for item in cur.fetchall()]
 
